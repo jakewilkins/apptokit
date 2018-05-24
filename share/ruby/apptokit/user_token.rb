7333ebd120b06149ee1654b43662e670e9ec5f7d
Jake Wilkins <jakewilkins@github.com>
2018-05-24 15:03:04 -0700

    user-token allows specifying an oauth code to exchange for a token
    Also allows an ENV passthrough that you can use in conjunction with `curl` to skip fetching a new token each time.

--- share/ruby/apptokit/user_token.rb
+++ share/ruby/apptokit/user_token.rb
@@ -7,17 +7,18 @@ require 'apptokit/oauth_callback_server'
 
 module Apptokit
   class UserToken
-    def self.generate(auto_open: true)
-      new(auto_open: auto_open).tap {|t| t.generate}
+    def self.generate(auto_open: true, code: nil)
+      new(auto_open: auto_open, code: code).tap {|t| t.generate}
     end
 
     attr_reader :auto_open, :installation_id, :mutex, :condition_variable
     attr_accessor :token, :token_type
     private :token=, :token_type=
 
-    def initialize(installation_id: nil, auto_open: true)
+    def initialize(installation_id: nil, auto_open: true, code: nil)
       @installation_id = installation_id || Apptokit.config.installation_id
       @auto_open = auto_open.nil? ? true : auto_open
+      @code = code
       @mutex, @condition_variable = Mutex.new, ConditionVariable.new
     end
 
@@ -28,6 +29,27 @@ module Apptokit
 
     def generate
       validate_generateable!
+
+      oauth_code = generate_oauth_code
+
+      token_info = exchange_code_for_token(oauth_code)
+      self.token = token_info["access_token"]
+      self.token_type = token_info["token_type"]
+      self
+    end
+
+    def client_id
+      Apptokit.config.client_id
+    end
+
+    def client_secret
+      Apptokit.config.client_secret
+    end
+
+    private
+
+    def generate_oauth_code
+      return @code if @code
 
       callback_server = OauthCallbackServer.new(mutex, condition_variable)
       callback_server.start
@@ -47,21 +69,8 @@ module Apptokit
         )
       end
 
-      token_info = exchange_code_for_token(callback_server.oauth_code)
-      self.token = token_info["access_token"]
-      self.token_type = token_info["token_type"]
-      self
+      callback_server.oauth_code
     end
-
-    def client_id
-      Apptokit.config.client_id
-    end
-
-    def client_secret
-      Apptokit.config.client_secret
-    end
-
-    private
 
     def exchange_code_for_token(code)
       uri = URI("#{Apptokit.config.github_url.to_s.gsub("api.", "")}/login/oauth/access_token?")
