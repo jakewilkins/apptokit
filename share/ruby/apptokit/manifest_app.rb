375527fda8d9988f2c58ea8051782429c361ab4c
Jake Wilkins <jakewilkins@github.com>
2020-02-20 11:00:25 -0800

    Pass callback_url with manifest

--- share/ruby/apptokit/manifest_app.rb
+++ share/ruby/apptokit/manifest_app.rb
@@ -115,6 +115,7 @@ module Apptokit
         name: yaml_manifest["name"] || generated_name,
         url: yaml_manifest["url"] || "http://example.com",
         hook_attributes: yaml_manifest["hook_attributes"] || {url: "http://example.com/webhooks"},
+        callback_url: yaml_manifest["callback_url"] || "http://localhost:8075/callback",
         redirect_url: callback_server_url,
         description: yaml_manifest["description"] || "An Apptokit Managed GitHub App",
         public: yaml_manifest["public"] || false,
