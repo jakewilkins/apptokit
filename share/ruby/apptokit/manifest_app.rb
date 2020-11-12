d35d3f3134701716fe815c01ea9a847ea31dcb4f
Jake Wilkins <jakewilkins@github.com>
2020-11-11 22:22:31 -0600

    Update ManifestApp

--- share/ruby/apptokit/manifest_app.rb
+++ share/ruby/apptokit/manifest_app.rb
@@ -1,133 +1,93 @@
 # frozen_string_literal: true
 
-require 'net/http'
-require 'erb'
-require 'tempfile'
-
-require 'apptokit/jwt'
-require 'apptokit/callback_server'
+require 'base64'
+require 'json'
+require 'apptokit/manifest_app/creator'
 
 module Apptokit
-  class ManifestApp
-    FORM_TEMPLATE = ERB.new(
-      Configuration::SHARE_DIR.join("create_manifest_app.html.erb").read
-    )
+  module ManifestApp
+    CACHE_DIR = Apptokit::ConfigLoader::HOME_DIR_CONF_DIR.join("manifest_apps")
 
-    attr_reader :env_name, :yaml_conf, :app_owner, :mutex, :condition_variable, :auto_open, :code, :skip_cache
-    attr_writer :code
-    private :code=
+    module_function
 
-    def initialize(env_name:, yaml_conf:, app_owner:, auto_open: nil, code: nil, skip_cache: false)
-      @env_name = env_name
-      @yaml_conf = yaml_conf
-      @app_owner = app_owner
-      @auto_open = auto_open.nil? ? true : auto_open
-      @code = code
-      @cached = true
-      @skip_cache = skip_cache
-      @mutex, @condition_variable = Mutex.new, ConditionVariable.new
-      @github_settings_response = nil
+    def load_from_cache(conf_loader)
+      return [{}, :unavailable] unless cache_path(conf_loader.env).exist?
+
+      opts = read_from_cache(conf_loader.env)
+
+      [convert_to_apptokit_opts(opts), opts]
     end
 
-    def create_app
-      walk_user_through_creation_flow unless code
-      @github_settings_response = exchange_code_for_app_settings
+    def create(conf_loader, cli_opts = {})
+      cli_opts[:config] = conf_loader
+
+      app_creator = ManifestApp::Creator.new(cli_opts)
+      settings = app_creator.create_app
+
+      persist_to_cache(conf_loader.env, settings)
+
+      load_from_cache(conf_loader, cli_opts)
     end
 
-    def walk_user_through_creation_flow
-      tempfile = Tempfile.new(["app_manifest", ".html"])
-      tempfile.write(FORM_TEMPLATE.result(binding))
-      tempfile.flush
+    def install(conf_loader, cli_opts, manifest_response = nil)
+      manifest_response ||= read_from_cache(conf_loader.env)
 
-      callback_server = CallbackServer.new(mutex, condition_variable, response: :manifest) do |server|
-        server.port = 8875
-        server.path = "/manifest_callback"
-      end
-      callback_server.start
+      installation_id = ManifestApp::Installer.call(
+        url: manifest_response['html_url'],
+        name: manifest_response['name'],
+        cli_opts: cli_opts
+      )
 
-      if auto_open
-        `$BROWSER #{tempfile.path}`
-      else
-        puts "Please open the link below to continue creating the application:\n\n  file://#{tempfile.path}\n\n"
-      end
+      manifest_response['installation_id'] = installation_id
+      persist_to_cache(conf_loader.env, manifest_response)
 
-      mutex.synchronize { condition_variable.wait(mutex, 60) }
-      callback_server.shutdown
-
-      if callback_server.killed?
-        $stderr.puts "Aborting manifest setup process, the App will not be created."
-        exit
-      end
-
-      unless callback_server.code
-        raise ApptokitError.new(
-          "Failed to get an manifest creation code from GitHub, did you visit the URL in your browser?",
-          type: :manifest_creation_failed
-        )
-      end
-
-      self.code = callback_server.code
-    ensure
-      if tempfile
-        tempfile.close
-        tempfile.unlink
-      end
+      load_from_cache(conf, cli_opts)
     end
 
-    def exchange_code_for_app_settings
-      uri = URI("#{Apptokit.config.github_api_url}/app-manifests/#{code}/conversions")
-      res = Net::HTTP.post(uri, "", { "Accept" => "application/vnd.github.fury-preview+json" })
+    def delete_cache(conf_loader)
+      path = cache_path(conf_loader.env)
+      return unless path.exist?
 
-      case res
-      when Net::HTTPSuccess
-        JSON.parse(res.body)
-      else
-        raise ApptokitError, "Failed to exchange GitHub App Manifest code for App credentials: #{res.code}\n\n#{res.body}"
-      end
+      FileUtils.rm(path)
     end
 
-    def manifest_json
-      JSON.pretty_generate({
-        name: yaml_manifest["name"] || generated_name,
-        url: yaml_manifest["url"] || "http://example.com",
-        hook_attributes: yaml_manifest["hook_attributes"] || { url: "http://example.com/webhooks" },
-        callback_url: yaml_manifest["callback_url"] || "http://localhost:8075/callback",
-        redirect_url: callback_server_url,
-        description: yaml_manifest["description"] || "An Apptokit Managed GitHub App",
-        public: yaml_manifest["public"] || false,
-        default_events: yaml_manifest["events"],
-        default_permissions: yaml_manifest["permissions"]
-      })
+    # private
+
+    def convert_to_apptokit_opts(raw_manifest)
+      hash = {
+        app_id:         raw_manifest["id"],
+        client_id:      raw_manifest["client_id"],
+        private_key:    OpenSSL::PKey::RSA.new(raw_manifest["pem"]),
+        client_secret:  raw_manifest["client_secret"],
+        webhook_secret: raw_manifest["webhook_secret"],
+      }
+      hash[:installation_id] = raw_manifest["installation_id"]
+
+      hash
     end
 
-    def generated_name
-      "#{env_name} Manifested App"
+    def read_from_cache(env)
+      JSON.parse(Base64.decode64(File.read(cache_path(env))))
     end
 
-    def callback_server_url
-      "http://localhost:8875/manifest_callback"
+    def persist_to_cache(env, settings)
+      ensure_cache_dir_exists!
+      File.write(cache_path(env), Base64.encode64(JSON.generate(settings)))
     end
 
-    def yaml_manifest
-      yaml_conf["manifest"] = fetch_manifest_from_url if yaml_conf["manifest_url"]
-      yaml_conf["manifest"]
+    def delete_cache(env)
+      ensure_cache_dir_exists!
+      FileUtils.rm(cache_path(env)) if cache_path.exist?
     end
 
-    def manifest_flow_start_url
-      owner_part = app_owner ? "/organizations/#{app_owner}" : ""
-      "#{Apptokit.config.github_url}#{owner_part}/settings/apps/new"
+    def cache_path(env)
+      @cache_path ||= CACHE_DIR.join("#{env}.yml")
     end
 
-    def fetch_manifest_from_url
-      res = Net::HTTP.get(URI(yaml_conf["manifest_url"]))
-
-      case res
-      when Net::HTTPSuccess
-        JSON.parse(res.body)
-      else
-        $stderr.puts "Could not fetch an App Manifest from #{yaml_conf['manifest_url']}"
-        exit 20
-      end
+    def ensure_cache_dir_exists!
+      return if @cache_dir_definitely_exists
+      FileUtils.mkdir_p(CACHE_DIR) unless CACHE_DIR.exist?
+      @cache_dir_definitely_exists = true
     end
   end
 end
