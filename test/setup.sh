278091479aa3e5fcf8eb1789d36b55c66c98af39
Jake Wilkins <jakewilkins@github.com>
2021-08-16 19:31:19 +0000

    install JWT during tests

--- test/setup.sh
+++ test/setup.sh
@@ -7,3 +7,7 @@ eval "echo \"$(cat test/env.yml)\"" > ./.apptokit.yml
 
 [ -f "./bats_private_key.pem" ] && rm "./bats_private_key.pem"
 echo "$BATS_PRIVATE_KEY" > ./bats_private_key.pem
+
+ruby_version_major=$(ruby --disable-gems -e "print RUBY_VERSION.split('.').tap {|a| a[2] = '0'}.join('.')")
+export GEM_PATH="$(pwd)/share/ruby/vendor/ruby/$ruby_version_major"
+gem install jwt
