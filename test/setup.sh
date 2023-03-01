e274c5e38dc2d10c0f52169efa94ff14a058a951
Jake Wilkins <jakewilkins@github.com>
2023-03-01 09:11:02 -0800

    try to install webrick now that it's a gem

--- test/setup.sh
+++ test/setup.sh
@@ -11,6 +11,7 @@ echo "$BATS_PRIVATE_KEY" > ./bats_private_key.pem
 ruby_version_major=$(ruby --disable-gems -e "print RUBY_VERSION.split('.').tap {|a| a[2] = '0'}.join('.')")
 export GEM_PATH="$(pwd)/share/ruby/vendor/ruby/$ruby_version_major"
 
+gem install webrick
 if [ ! -d $GEM_PATH ]; then
   gem install jwt 2>&1 1>/dev/null
 fi
